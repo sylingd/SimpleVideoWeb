c0d120c52c1414be113a0f695bfea2770b540b04
ShuangYa <896640547@qq.com>
2019-04-22 23:39:04 +0800

    fixed router

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './frontend/App';
-import Home from './frontend/Home';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Router>
-    <Route path="/" component={App}>
-      <Route path="/" component={Home}/>
-    </Route>
+    <App />
   </Router>,
   document.getElementById('root') as HTMLElement
 );
