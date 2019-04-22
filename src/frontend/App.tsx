ead0fd92169b4ee54dedfdba9c4704111f83a783
ShuangYa <896640547@qq.com>
2019-04-23 00:04:04 +0800

    Update

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -2,13 +2,21 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
+import { Layout } from 'antd';
+
+const {
+  Header, Content
+} = Layout;
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <Route path="/" component={Home}/>
-      </div>
+      <Layout className="App">
+        <Header></Header>
+        <Content>
+          <Route path="/" component={Home}/>
+        </Content>
+      </Layout>
     );
   }
 }
