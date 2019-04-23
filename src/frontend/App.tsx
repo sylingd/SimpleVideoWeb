2d50e5419b50eb875d8fc5dca83df32bf0eb2f34
ShuangYa <896640547@qq.com>
2019-04-23 09:50:23 +0800

    Bugfix

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -2,19 +2,29 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
-import { Layout } from 'antd';
+import { Layout, Menu } from 'antd';
 
 const {
   Header, Content
 } = Layout;
 
+const SubMenu = Menu.SubMenu;
+
 class App extends React.Component {
   public render() {
     return (
-      <Layout className="App">
-        <Header></Header>
-        <Content>
-          <Route path="/" component={Home}/>
+      <Layout className="app">
+        <Header>
+          <Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }}>
+            <Menu.Item key="main">主站</Menu.Item>
+            <SubMenu title="分类">
+              <Menu.Item key="cat:1">分类1</Menu.Item>
+              <Menu.Item key="cat:2">分类2</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Header>
+        <Content className="content">
+          <Route path="/" component={Home} />
         </Content>
       </Layout>
     );
